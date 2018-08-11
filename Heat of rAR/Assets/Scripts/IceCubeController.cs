1c1d7a9096ef8311301fda50f47090da3e10bd67
Leonid <leonidb93@gmail.com>
2018-08-11 19:46:58 +0200

    add anubis

--- Heat of rAR/Assets/Scripts/IceCubeController.cs
+++ Heat of rAR/Assets/Scripts/IceCubeController.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IceCubeController : MonoBehaviour {
+    private int myId;
     public float speed;
     private Rigidbody rb;
+    private Transform destination;
+    private float distanceToStop = 0.1f;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+        destination = GameObject.Find("MoveToPlayer").transform;
         rb = GetComponent<Rigidbody>();
+        myId = (int)Random.Range(0, 10);
+        if (myId == ShamanController.shamanMain.ShamanNumber)
+        {
+            ShamanController.shamanMain.Activate();
+            destination = ShamanController.shamanMain.evilPosition;
+            Vector2 direction = Random.insideUnitCircle.normalized;
+            destination.Translate(new Vector3(direction.x * 0.1f, 0.0f, direction.y * 0.1f));
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        rb.AddForce(new Vector3(speed * Time.deltaTime, 0, 0));
 
+        float step = speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, destination.position) > distanceToStop)
+        {
+            transform.LookAt(destination);
+            rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+        } else
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
     }
+
+
+
+
+
+
 }
---
+++ Heat of rAR/Assets/Scripts/ShamanController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShamanController : MonoBehaviour {
+    public static ShamanController shamanMain;
+
+    private MeshRenderer mr;
+    public readonly int ShamanNumber = 7;
+    private bool activated = false;
+    public Transform evilPosition;
+
+    //! Makes sure that there is only one instance of the static class object
+    private void Awake()
+    {
+            shamanMain = this;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        mr = GetComponent<MeshRenderer>();
+        mr.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Activate()
+    {
+        if (!activated)
+        {
+
+            activated = true;
+            float x = Random.Range(-0.5f, 0.5f);
+            float z = Random.Range(-0.3f, 0.3f);
+            Vector3 pos = new Vector3(x, 0.0f, z);
+            transform.localPosition = pos;
+            evilPosition = transform;
+            mr.enabled = true;
+        }
+
+    }
+}
