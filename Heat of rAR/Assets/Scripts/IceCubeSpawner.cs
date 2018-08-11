19ebd3371d95659c14f6ef8137ef952bf6883ecb
Dean <deanjuerges@posteo.de>
2018-08-11 14:57:01 +0200

    Added IceCubSpawner prefab to randomly spawn ice cube prefabs regulary

---
+++ Heat of rAR/Assets/Scripts/IceCubeSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCubeSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject icecubePrefab;
+
+    [SerializeField]
+    private float spawnTime = 1.0f;
+
+    [SerializeField]
+    private float gameWidth = 10.0f;
+
+    [SerializeField]
+    private float gameHeight = 10.0f;
+
+    private float lastSpawnTime;
+
+	// Use this for initialization
+	void Start () {
+        lastSpawnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time - lastSpawnTime > spawnTime)
+        {
+            float x = Random.Range(-gameWidth / 2.0f, gameWidth / 2.0f);
+            float z = Random.Range(-gameHeight / 2.0f, gameHeight / 2.0f);
+            GameObject icecube = Instantiate(icecubePrefab, new Vector3(x, 0.0f, z), Quaternion.identity);
+            lastSpawnTime = Time.time;
+        }
+	}
+}
